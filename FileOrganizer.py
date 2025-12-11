9a66e5ca1b1c7dd3899aa73c1095eeefadb2296f
amir miri <saturnloop.ai@gmail.com>
2025-12-11 21:27:21 +0330

    adding base and target directories

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -2,3 +2,8 @@
 from encodings.base64_codec import base64_decode
 from pathlib import Path
 import shutil
+
+
+# base  directory
+base_dir = Path("/home/amirmiri/Desktop")
+target_dir = base_dir / "sorted"

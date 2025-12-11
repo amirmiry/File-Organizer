877cd55c4c4032555b4c2fd6776860623411b5a3
amir miri <saturnloop.ai@gmail.com>
2025-12-11 21:28:25 +0330

    adding File Categorise

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -3,7 +3,15 @@ from encodings.base64_codec import base64_decode
 from pathlib import Path
 import shutil
 
-
 # base  directory
 base_dir = Path("/home/amirmiri/Desktop")
 target_dir = base_dir / "sorted"
+
+# declare categories and extensions
+FILE_CATEGORIES = {
+    "images": [".jpj", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg", ".webp" ],
+    "documents":[".pdf" ,".doc" ,".dox" , ".txt",".xls" ,".xlsx" ,".ppt" ,".pptx"],
+    "videos": [ ".mp4" ,".mkv" ,".avi" ,".mov" ,".wmv" ],
+    "audio": [ ".mp3",  ".wav", ".aac" , ".flac" , ".ogg"],
+    "archives": [ ".zip",  ".rar" , ".tar" , ".gz", ".7z"]
+}
